5cd9336f2d1b0a4e05bb88cbc1f621aa35d44c92
salaheddinerossi <salaheddine.rossi@etu.uae.ac.ma>
2023-10-21 12:55:54 +0100

    test1 pipe

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const http = require('http');
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello, World! , salaaaah');
+    res.end('Hello, World! , hii');
 });
 
 server.listen(3000, '0.0.0.0', () => {
